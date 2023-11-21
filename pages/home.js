5205bc7bd811eecdb99b3f4501d61f8afc3b82a4
ArtemNikov <sartem.meshkov@gmail.com>
2023-11-21 12:13:31 -0800

    added buttons

--- pages/home.js
+++ pages/home.js
@@ -1,9 +1,39 @@
 import React from 'react'
 import style from "../src/app/home.module.scss"
+import Router from 'next/router';
 
 const home = () => {
+
+  const buttonList = [
+    {
+      name: "PVP",
+      action: () => Router.push("/game")
+    },
+    {
+      name: "PVE",
+      action: () => Router.push("/game")
+    },
+    {
+      name: "LEADER BOARD",
+      action: () => { // TODO - make it open a dialog }
+    },
+    {
+      name: "PLAYER ЧТО ТО ТАМ",
+      action: () => { // I have no idea what is that }
+    },
+    {
+      name: "PRACTICE",
+      action: () => Router.push("/practice")
+    }
+  ]
+
   return (
     <div className={style.main}>
+      {buttonList.map(item => {
+        <button onClick={item.action}>
+          {item.name}
+        </button>
+      })}
     </div>
   )
 }
