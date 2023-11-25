cc1232c932e9049dc7744f724cb1d27c11981b29
ArtemNikov <sartem.meshkov@gmail.com>
2023-11-25 10:07:22 -0800

    fix

--- utils/axios.js
+++ utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.API_URL || "https://digital-server-5szy7kxnra-uc.a.run.app/"
+const API_URL = "https://digital-server-5szy7kxnra-uc.a.run.app/"
 
 const instance = axios.create({
   baseURL: API_URL,
