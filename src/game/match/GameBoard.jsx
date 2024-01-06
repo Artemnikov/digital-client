a3b4322c44264d121cf63918a78b756b2d385fe2
ArtemNikov <sartem.meshkov@gmail.com>
2024-01-06 05:49:44 -0800

    added layout elements

--- src/game/match/GameBoard.jsx
+++ src/game/match/GameBoard.jsx
@@ -1,21 +1,47 @@
 import { useSelector } from "react-redux"
 import style from "./match.module.scss"
 import Image from "next/image"
+import { useState } from "react"
 
 
 const GameBoard = () => {
   const gameData = useSelector(state => state.game.gameData)
   console.log(gameData[0])
   const currentUser = "x"
+  const [isCommited, setIsCommited] = useState(false)
+
+  const handleCommit = () => {
+    setIsCommited(!isCommited)
+    // dispatch a ws message
+  }
 
   return (
     <div className={style.main}>
+      <div className={style.layout}>
+        <div className={style.hero}>
+          <Image src="/game/layout/dashboard_stats.png" width={500} height={500} />
+        </div>
+        <div className={style.power}>
+          <Image src="/game/layout/dashboard_ep.png" width={300} height={300} />
+        </div>
+        <div className={style.commitBtn} disabled={isCommited} onClick={handleCommit}>
+          {isCommited
+            ? <Image src="/game/layout/btn_commit_pressed.gif" width={200} height={100} />
+            : (
+              <>
+                <Image src="/game/layout/btn_commit.png" width={200} height={100} />
+                <p className={style.commitTxt}>COMMIT</p>
+              </>
+            )
+          }
+        </div>
+      </div>
       <div className={style.board}>
         {gameData.map(item => (
           <div key={item.id} className={`${style.tile} ${!item.exist && style.hide}`}>
             {item.player_id === currentUser
-              ? <Image className={`${item.orientation === 1 && style.flip}`} width={200} height={200} src="game/container_green.svg" />
-              : <Image className={`${item.orientation === 1 && style.flip}`} width={200} height={200} src="/game/container_blue.svg" />
+              ? <Image className={`${item.orientation === 1 && style.flip}`} width={200} height={200} src="game/board/container_green.svg" />
+              : <Image className={`${item.orientation === 1 && style.flip}`} width={200} height={200} src="/game/board/container_blue.svg" />
             }
           </div>
         ))}
